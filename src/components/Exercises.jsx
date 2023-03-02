163ee18522a659a46b77b6278a08f9ecf6cbee6c
Jarno02 <jarno.ryhanen02@gmail.com>
2023-03-02 11:12:14 +0200

    Fixed horizontal scrollbar not updating the exercises

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -5,8 +5,7 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from './ExerciseCard';
 
 
-const Exercises = ({ exercises, bodypart, setExcercises }) => {
-
+const Exercises = ({ exercises, bodyPart, setExercises }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisesPerPage = 9;
 
@@ -18,6 +17,24 @@ const Exercises = ({ exercises, bodypart, setExcercises }) => {
     setCurrentPage(value);
     window.scrollTo({ top: 1800, behavior: "smooth" });
   }
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData("https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions);
+      }
+      setExercises(exercisesData);
+    }
+
+    fetchExercisesData();
+  }, [bodyPart])
+  
   return (
     <Box
       id="exercises"
